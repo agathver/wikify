218d99a5c640a9ffc7eea3f0c0e45838e9741535
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2016-11-24 12:27:50 +0530

    add passportjs and node-acl authentication schemes

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports =  {
-	server : require('./server'),
+    server : require('./server'),
     secrets: require('./secrets'),
     db: require('./db'),
     site: require('./site'),
- protection: require('./protection')
+    protection: require('./protection')
 }
